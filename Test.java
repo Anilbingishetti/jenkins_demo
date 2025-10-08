9ccbba36c29c1340232c8ebebda3a9ebc8c20180
Bingishetti Anil kumar <bingishettianilkumar23@gmail.com>
2025-10-08 16:11:00 +0530

    this is sample

--- Test.java
+++ Test.java
@@ -1,6 +1,8 @@
 class Test {
     public static void main(String[] args) {
-      //sum of five numbers
-      System.out.println("Hello")
+      int [] arr = {1, 2, 3, 4, 5};
+      for (int i = 0; i < arr.length; i++) {
+        System.out.println(arr[i]);
+      }
     }
 }   
