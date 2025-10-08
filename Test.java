2a12bf3b07bade45e8137179a3d6773175bc78f4
Bingishetti Anil kumar <bingishettianilkumar23@gmail.com>
2025-10-08 11:48:40 +0530

    this is second commit

--- Test.java
+++ Test.java
@@ -1,13 +1,18 @@
 import java.util.*;
 class Test {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("apple");
-        list.add("banana");
-        list.add("cherry");
-
-        for (String fruit : list) {
-            System.out.println(fruit);
+      //sum of five numbers
+      int [] arr = new int [5];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter five numbers:");
+        for(int i=0;i<5;i++){
+            arr[i] = sc.nextInt();
         }
+        int sum = 0;
+        for(int i=0;i<5;i++){
+            sum += arr[i];
+        }
+        System.out.println("Sum of five numbers is: " + sum);
+        sc.close();
     }
 }   
