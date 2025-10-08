73c451b26973d3e997d8dc6f39964733f76805ba
Bingishetti Anil kumar <bingishettianilkumar23@gmail.com>
2025-10-08 16:26:41 +0530

    the second sample

--- Test.java
+++ Test.java
@@ -1,8 +1,11 @@
 class Test {
     public static void main(String[] args) {
-      int [] arr = {1, 2, 3, 4, 5};
-      for (int i = 0; i < arr.length; i++) {
-        System.out.println(arr[i]);
-      }
+      //sum of natural numbers
+        int n = 10;
+        int sum = 0;
+        for(int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of first " + n + " natural numbers is: " + sum);
     }
 }   
