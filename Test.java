7a20a8a5c2f7b806f91af9e8801103aeba628f65
SindhuManga <mangasindhu2303@gmail.com>
2025-10-08 12:10:12 +0530

    this is 3rd

--- Test.java
+++ Test.java
@@ -1,18 +1,6 @@
-import java.util.*;
 class Test {
     public static void main(String[] args) {
       //sum of five numbers
-      int [] arr = new int [5];
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter five numbers:");
-        for(int i=0;i<5;i++){
-            arr[i] = sc.nextInt();
-        }
-        int sum = 0;
-        for(int i=0;i<5;i++){
-            sum += arr[i];
-        }
-        System.out.println("Sum of five numbers is: " + sum);
-        sc.close();
+      System.out.println("Hello");
     }
 }   
