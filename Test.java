d9225264584620342c2a97496fdb4a334a96563b
Bingishetti Anil kumar <bingishettianilkumar23@gmail.com>
2025-10-09 09:17:40 +0530

    test

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ class Test {
       //sum of natural numbers
         int n = 15;
         int sum = 0;
-        for(int i = 1; i <= n; i++) {
+        for(int i = 0; i <= n; i++) {
             sum += i;
         }
         System.out.println("Sum of first " + n + " natural numbers is: " + sum);
