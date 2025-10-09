6a374fa54c3d21e910f0f737f3533c124b80dc4f
Bingishetti Anil kumar <bingishettianilkumar23@gmail.com>
2025-10-09 10:26:43 +0530

    testing web hook

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 class Test {
     public static void main(String[] args) {
       //sum of natural numbers
-        int n = 15;
+        int n = 20;
         int sum = 0;
         for(int i = 0; i <= n; i++) {
             sum += i;
